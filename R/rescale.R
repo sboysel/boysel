06abb23abceac4c4fea853db3624d5b5ef5a0d5f
Sam Boysel <sboysel@gmail.com>
2016-07-06 20:49:43 -0400

    use norm in rescale(x, method = 'unit.length')

--- R/rescale.R
+++ R/rescale.R
@@ -4,7 +4,11 @@
 #' @param method character string specifying the type of scaling.  Currently 
 #' supported options include 'standardize', 'unit.range', and 'unit.length'.
 #' @param ... additional parameters passed to specific rescaling functions 
-#' (e.g. \code{na.rm}).
+#' (e.g. \code{na.rm}).  Note that if \code{x} contains \code{NA}s, 
+#' \code{rescale(x, method = "standardize", na.rm = TRUE)} and \code{rescale(x, 
+#' method = "unit.range")} will not drop \code{NA}s in the resulting scaled 
+#' vector while \code{rescale(x, method = "standardize")} will stop with an
+#' error.
 #'
 #' @return a numeric vector of equal length as x, rescaled according to the 
 #' procedure chosen in 'type'.
@@ -35,10 +39,13 @@ rescale <- function(x, method = "standardize", ...) {
   if (! method %in% c("standardize", "unit.range", "unit.length")) {
     stop("method must be one of 'standardize', 'unit.range', and 'unit.length'.")
   }
+  if (method == "unit.length" & NA %in% x) {
+    stop("Cannot rescale x to unit length with NA values")
+  }
   switch(method,
          standardize = scale(x, ...),
          unit.range = (x - min(x, ...)) / (max(x, ...) - min(x, ...)),
-         unit.length = x / sqrt(sum(x^2, ...))
+         unit.length = x / norm(x, type = "2")
   )
 }
 
