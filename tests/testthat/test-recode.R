702ba35d712d59361c8c948d98c7133dd2a429e9
Sam Boysel <sboysel@gmail.com>
2016-07-22 12:40:18 -0400

    adding basic tests for recode()

--- tests/testthat/test-recode.R
+++ tests/testthat/test-recode.R
@@ -1,7 +1,19 @@
 context("Recode")
 
-test_that("recode returns appropriate object types", {
-  x <- c(1:5, NA)
+x <- c(1:5, NA)
+
+test_that("recode()", {
   r <- recode(x, `2` = c(3, 5), `1` = c(NA, 1), .na = NULL, .default = 3)
+  rr <- recode(x, `2` = 1, `1` = 2, .na = NA, factor = TRUE)
+  expect_identical(x, recode(x))
   expect_identical(r, c("1", "3", "2", "3", "2", "1"))
+  expect_identical(boysel::as_numeric(rr), c(2, 1, 3, 4, 5, NA))
+  expect_is(rr, "factor")
+  expect_message(recode(x), "No default value set")
+})
+
+test_that("binary_recode()", {
+  r <- binary_recode(x, c(1, 2))
+  expect_is(r, "integer")
+  expect_identical(r, as.integer(c(1, 1, 0, 0, 0, NA)))
 })
