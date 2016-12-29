248fe66f55f737c30b794a7d277e144860cc5dee
Sam <sboysel@gmail.com>
2016-12-28 23:09:34 -0800

    adding group_demean tests

--- tests/testthat/test-demean.R
+++ tests/testthat/test-demean.R
@@ -1,11 +1,25 @@
 context("Group de-mean")
 
+x <- 1:10
+g <- rep(c("a", "b"), 5)
+d <- data.frame(x = x, g = g)
+
+gm <- rep(c(5, 6), 5)
+demeaned.manual <- x - gm
+
 test_that("Group de-mean via linear model is identical to manual method", {
-  x <- 1:10
-  g <- rep(c("a", "b"), 5)
-  gm <- rep(c(5, 6), 5)
-  demeaned.manual <- x - gm
-  expect_equal(demeaned.manual, group_demean(x = x, groups = g))
-  d <- data.frame(x = x, g = g)
-  expect_equal(demeaned.manual, group_demean(f = x ~ g, data = d))
+  expect_equal(demeaned.manual, group_demean(x = x, groups = g), tolerance = .Machine$double.eps ^ 0.5)
+  expect_equal(demeaned.manual, group_demean(f = x ~ g, data = d), tolerance = .Machine$double.eps ^ 0.5)
+})
+
+test_that("Vectors are returned", {
+  expect_is(group_demean(x = x, groups = g), "numeric")
+  expect_is(group_demean(f = x ~ g, data = d), "numeric")
+})
+
+test_that("Retaining names works", {
+  xx <- group_demean(x = x, groups = g, named = TRUE)
+  dd <- group_demean(f = x ~ g, data = d, named = TRUE)
+  expect_equal(names(xx), as.character(x))
+  expect_equal(names(dd), as.character(d$x))
 })
