232091388c400862dfd0ef61b74b61d2289fe39a
Sam Boysel <sboysel@gmail.com>
2017-06-02 22:22:01 -0700

    update documentation

--- R/statfuns.R
+++ R/statfuns.R
@@ -20,14 +20,15 @@ cv <- function(x, na.rm = FALSE) {
   stats::sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
 }
 
-#' Statistical Mode
+#' Calculate most frequently occuring value in a vector
 #' 
-#' Calculate most frequently occuring value in a vector.
+#' Calculate most frequently occuring value in a vector (statistical mode).
 #' 
-#' @param x a vector
+#' @param x a vector.
 #' @param ... additional parameters passed on to \code{table}.
-#' @return a scalar equal to the modal value of \code{x}.  If \code{x} is multimodal, \code{length(x)}
-#' will equal the number of unique modes in \code{x}.
+#' @return a scalar equal to the modal value of \code{x}.  If 
+#' \code{x} is multimodal, \code{length(x)} will equal the number 
+#' of unique modes in \code{x}.
 #' @references \url{http://mathworld.wolfram.com/Mode.html}
 #' 
 #' @examples
@@ -40,30 +41,30 @@ cv <- function(x, na.rm = FALSE) {
 #' xx <- c(1, x)
 #' most_freq(xx)
 #' 
+#' @name most_freq
 #' @export
-#' @rdname most_freq
 most_freq <- function(x, ...) {
   UseMethod("most_freq")
 }
 
-#' @export
 #' @rdname most_freq
+#' @export
 most_freq.numeric <- function(x, ...) {
   tab <- table(x, ...)
   m <- which(tab == max(tab, na.rm = TRUE), arr.ind = TRUE)
   as_numeric(names(tab)[m])
 }
 
-#' @export
 #' @rdname most_freq
+#' @export
 most_freq.character <- function(x, ...) {
   tab <- table(x, ...)
   m <- which(tab == max(tab, na.rm = TRUE), arr.ind = TRUE)
   names(tab)[m]
 }
 
-#' @export
 #' @rdname most_freq
+#' @export
 most_freq.factor <- function(x, ...) {
   tab <- table(x, ...)
   m <- which(tab == max(tab, na.rm = TRUE), arr.ind = TRUE)
