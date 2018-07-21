5177185216fdfadcc4a535acb5b5a872050ccb4c
Sam Boysel <sboysel@gmail.com>
2018-07-20 19:28:57 -0700

    refactor insertDashesAddin

--- R/addins.R
+++ R/addins.R
@@ -1,12 +1,30 @@
 # I map to Ctrl+Shift+- using Tools > Modify Keyboard Shortcuts...
 insertDashesAddin <- function() {
+  
   con <- rstudioapi::getActiveDocumentContext()
+  
+  pos <- make_position(con = con)
+  
+  rstudioapi::insertText(
+    location = pos,
+    text = make_dashes(col = col),
+    id = con$id
+  )
+  
+}
+
+make_position <- function(con) {
+  
   row <- con$selection[[1]]$range$start["row"]
   col <- con$selection[[1]]$range$start["column"]
-  pos <- rstudioapi::document_position(row,
-                                      (col + 1))
-  dashes <- paste0(" ", paste0(rep("-", 80 - col), collapse = ""))
-  rstudioapi::insertText(location = pos,
-                         text = dashes,
-                         id = con$id)
+  
+  rstudioapi::document_position(
+    row = row,
+    col = (col + 1)
+  )
+  
+}
+
+make_dashes <- function(col, max_width = 80) {
+  paste0(" ", paste0(rep("-", max_width - col), collapse = ""))
 }
